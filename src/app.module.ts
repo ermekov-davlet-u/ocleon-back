1929f8bda61917bfde358394b58581d4826a1658
ermekov-davlet-u <d.ermekovv@gmail.com>
2026-01-31 15:07:32 +0600

    add progect

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { Todo } from './todos/entities/todo.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: 'postgresql://postgres:[YOUR-PASSWORD]@db.eajbhliparlkciiepjjx.supabase.co:5432/postgres',
+      url: 'postgresql://postgres:p!$c6H8$A@tepjW@db.eajbhliparlkciiepjjx.supabase.co:5432/postgres',
       entities: [Todo],
       synchronize: true,
       extra: {
