d9f38e8f690b55df8d83cf87f26f1f107effdcc6
ermekov-davlet-u <d.ermekovv@gmail.com>
2026-02-14 21:58:43 +0600

    add material in  invoice-item

--- src/invoice-item/invoice-item.module.ts
+++ src/invoice-item/invoice-item.module.ts
@@ -4,9 +4,10 @@ import { InvoiceItemController } from './invoice-item.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { InvoiceItem } from './entities/invoice-item.entity';
 import { Invoice } from 'src/invoice/entities/invoice.entity';
+import { Material } from 'src/material/entities/material.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([InvoiceItem, Invoice])],
+  imports: [TypeOrmModule.forFeature([InvoiceItem, Invoice, Material])],
   controllers: [InvoiceItemController],
   providers: [InvoiceItemService],
 })
