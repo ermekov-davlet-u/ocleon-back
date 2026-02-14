2db21a5008e076af3edf1c634d4a6588bcc800bf
ermekov-davlet-u <d.ermekovv@gmail.com>
2026-02-14 21:51:41 +0600

    add invoice item

--- src/invoice-item/invoice-item.module.ts
+++ src/invoice-item/invoice-item.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { InvoiceItemService } from './invoice-item.service';
 import { InvoiceItemController } from './invoice-item.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InvoiceItem } from './entities/invoice-item.entity';
+import { Invoice } from 'src/invoice/entities/invoice.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([InvoiceItem, Invoice])],
   controllers: [InvoiceItemController],
   providers: [InvoiceItemService],
 })
